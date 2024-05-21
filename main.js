39bf25f023316da8399497d9c9f17c345fca8a1a
Sm Kou <66178856+SmKou@users.noreply.github.com>
2024-05-21 16:04:04 -0700

    WIP Add interface

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+import p5 from 'p5'
 import './style.css'
+
+const setup = (p) => {
+    p.createCanvas(p.windowWidth, p.windowHeight)
+}
+
+const draw = (p) => {
+    p.background(20)
+}
+
+const sketch = p => {
+    p.setup = setup(p)
+    p.draw = draw(p)
+    p.windowResized = () => {
+        p.resizeCanvas(p.windowWidth, p.windowHeight)
+    }
+}
+new p5(sketch)
