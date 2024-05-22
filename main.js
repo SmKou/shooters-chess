68c18d1353981902fbf8cb80ed7136593a50d656
Sm Kou <66178856+SmKou@users.noreply.github.com>
2024-05-21 17:06:40 -0700

    Add chessboard labels

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
-import p5 from 'p5'
 import './style.css'
 
-const setup = (p) => {
-    p.createCanvas(p.windowWidth, p.windowHeight)
+const green = '#385546'
+const beige = '#e9d7bc'
+const lbl_color = '#455c52'
+
+const canvas = document.getElementById('game')
+canvas.width = canvas.clientWidth
+canvas.height = canvas.clientHeight
+const ctx = canvas.getContext("2d")
+
+const size_canvas = () => {
 }
 
-const draw = (p) => {
-    p.background(20)
-}
+const draw_board = () => {
+    const a_code = 65
+    const max_squ = 8     
+    const squ = (canvas.clientWidth - 72) / 8
+    const edge = 36
+    const offset = squ / 2
 
-const sketch = p => {
-    p.setup = setup(p)
-    p.draw = draw(p)
-    p.windowResized = () => {
-        p.resizeCanvas(p.windowWidth, p.windowHeight)
+    ctx.font = '28px sans-serif'
+    ctx.textAlign = 'center'
+    ctx.fillStyle = lbl_color
+    for (let i = 0; i < max_squ; ++i) {
+        ctx.fillText((8 - i) + "", edge / 2, edge + (i * squ) + offset)
+        ctx.fillText(String.fromCharCode(a_code + i), edge + (i * squ) + offset, canvas.clientHeight - (edge / 2))
     }
 }
-new p5(sketch)
+draw_board()
+
