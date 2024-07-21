f22ddb11f4740799b1fe5035b3342fdc1e263b69
SmKou <66178856+SmKou@users.noreply.github.com>
2024-07-20 17:06:11 -0700

    Move validation and actions into controller (abstract)

--- prototype/main.js
+++ prototype/main.js
@@ -73,13 +73,28 @@ const pos_idx = (coord) => alpha[coord[0]] + 8 * (Number(coord[1]) - 1)
 
 const format_name = (name, pos) => {
 	const piece = name.split('_')[1]
-	return `${name[0].toUpperCase() + name.slice(1)} at ${pos.toUpperCase()}`
+	return `${piece[0].toUpperCase() + piece.slice(1)} at ${pos.toUpperCase()}`
 }
 
 const move = (pre, post) => {
+	const idx = pos_idx(pre)
+	const piece = game.board[idx]
+
+	const dest_idx = pos_idx(post)
+	const dest_piece = game.board[dest_idx]
+
+	if (dest_piece)
+		return {
+			status: false,
+			message: `${format_name(piece, pre)} cannot move to ${post}`
+		}
+
 	return {
 		status: true,
-		message: `${pre} moved to ${post}`
+		message: `${format_name(piece, pre)} moved to ${post}`,
+		deselect: [pre],
+		move: { [idx]: dest_idx },
+		piece
 	}
 }
 
@@ -120,6 +135,29 @@ const unload = (pre, post) => {
 	}
 }
 
+const render_piece = (div, piece) => {
+	const span = document.createElement('span')
+	span.setAttribute('class', 'piecename')
+	span.append(
+		document.createTextNode(
+			piece[1] === 'knight' ? 'N' : piece[1][0].toUpperCase()
+		)
+	)
+	div.append(span)
+
+	const superscript = document.createElement('span')
+	superscript.setAttribute('class', 'superscript')
+	superscript.append(document.createTextNode(piece[0][0].toUpperCase()))
+	div.append(superscript)
+
+	if (piece.length > 2) {
+		const subscript = document.createElement('span')
+		subscript.setAttribute('class', 'subscript')
+		subscript.append(document.createTextNode(piece[2][0].toUpperCase()))
+		div.append(subscript)
+	}
+}
+
 const draw = () => {
 	document.getElementById('board').innerHTML = ''
 	for (let i = 0; i < max_squ ** 2; ++i) {
@@ -128,28 +166,7 @@ const draw = () => {
 		div.id = `${Object.keys(alpha)[idx_val]}${8 - Math.floor(i / max_squ)}`
 		if (game.board[i]) {
 			const piece = game.board[i].split('_')
-
-			const span = document.createElement('span')
-			span.setAttribute('class', 'piecename')
-			span.append(
-				document.createTextNode(
-					piece[1] === 'knight' ? 'N' : piece[1][0].toUpperCase()
-				)
-			)
-			div.append(span)
-
-			const superscript = document.createElement('span')
-			superscript.setAttribute('class', 'superscript')
-			superscript.append(document.createTextNode(piece[0][0].toUpperCase()))
-			div.append(superscript)
-
-			if (piece.length > 2) {
-				const subscript = document.createElement('span')
-				subscript.setAttribute('class', 'subscript')
-				subscript.append(document.createTextNode(piece[2][0].toUpperCase()))
-				div.append(subscript)
-			}
-
+			render_piece(div, piece)
 		}
 		document.getElementById('board').append(div)
 	}
@@ -158,17 +175,19 @@ const draw = () => {
 }
 draw()
 
+const deselect = (pos) => {
+	document.getElementById(pos).style.background = 'transparent'
+	const piece = game.board[pos_idx(pos)]
+	const bridged = game.pieces[piece].bridged
+	if (bridged)
+		document.getElementById(bridged).style.background = 'transparent'
+}
+
 const select = (pos) => {
 	if (pos === game.pos) return;
 
 	if (game.pos) {
-		document.getElementById(game.pos).style.background = 'transparent'
-
-		const piece = game.board[pos_idx(game.pos)]
-		const bridged = game.pieces[piece]?.bridged
-		if (bridged)
-			document.getElementById(bridged).style.background = 'transparent'
-
+		deselect(game.pos)
 		game.pos = ''
 	}
 
@@ -184,7 +203,8 @@ const select = (pos) => {
 }
 
 const command = []
-const controller = (pre, post, cmd) => {
+
+const actions_controller = (pre, post, cmd) => {
 	switch (cmd) {
 		case 'm':
 			return move(pre, post)
@@ -198,26 +218,61 @@ const controller = (pre, post, cmd) => {
 			else
 				return unbridge(pre, post)
 		default:
-			return { status: false, message: `Improper input: ${pre} ${cmd} ${post}`}
+			return {
+				status: false,
+				message: `Improper maneuver: ${pre}${cmd}${post}`
+			}
 	}
 }
 
-const change_board = (changes) => {
-	for (const coord of changes.remove) {
+const controller = ({ piece, actions, targets }) => {
+	const changes = {
+		move: [],
+		remove: [],
+		message: []
+	}
+
+	let target = targets.shift()
+	while (actions.length) {
+		const action = actions.shift()
+		const res = actions_controller(piece, target, action)
+		if (!res.status)
+			return res
+
+		if (res.piece)
+			piece = res.piece
+		if (res.move)
+			changes.move.push(res.move)
+		if (res.remove)
+			changes.remove.push(res.remove)
+
+		if (targets.length)
+			target = targets.shift()
+		changes.message.push(res.message)
+	}
+
+	return changes
+}
+
+const change_board = ({ move, remove }) => {
+	for (const coord of remove) {
 		const idx = pos_idx(coord)
 		const piece = game.board[idx]
 		delete(game.pieces[piece])
 		game.board[idx] = ''
 	}
 
-	for (const coord of Object.keys(changes.move)) {
+	for (const coord of Object.keys(move)) {
 		const idx = pos_idx(coord)
 		const piece = game.board[idx]
 
-		const dest_idx = pos_idx[changes.move[coord]]
+		const dest_idx = pos_idx[move[coord]]
 		const dest_piece = game.board[dest_idx]
 		if (dest_piece)
-			return { status: false, message: `Error found: ${dest_piece} at ${changes.move[coord]}, ${piece} cannot move.` }
+			return {
+				status: false,
+				message: `Error found: ${dest_piece} at ${move[coord]}, ${piece} cannot move.`
+			}
 
 		game.board[idx] = ''
 		game.board[dest_idx] = piece
@@ -229,8 +284,8 @@ const change_board = (changes) => {
 const clear = (message) => {
 	if (message)
 		document.getElementById('confirmation').innerHTML = message
-		document.getElementById('user-command').value = ''
-		command.length = 0
+	document.getElementById('user-command').value = ''
+	command.length = 0
 }
 
 document.getElementById('user-command').addEventListener('keydown', e => {
@@ -254,15 +309,16 @@ document.getElementById('user-command').addEventListener('keydown', e => {
 
 	// Command execution
 	document.getElementById('confirmation').innerHTML = ''
+	const cmd = command.join('')
 
 	if (command.length > 5) {
-		clear(`Invalid maneuver: ${command.join('')}`)
+		clear(`Invalid maneuver: ${cmd}`)
 		return;
 	}
 
 	for (const token of command)
 		if (token.length < 1 || token.length > 2) {
-			clear(`Invalid maneuver: ${command.join('')}`)
+			clear(`Invalid maneuver: ${cmd}`)
 			return;
 		}
 
@@ -272,48 +328,23 @@ document.getElementById('user-command').addEventListener('keydown', e => {
 		targets: []
 	}
 
-	while (command.length) {
+	let counts = []
+	while (command.slice().length) {
 		const token = command.shift()
 		if (token.length === 1)
 			tokens.actions.push(token)
 		else
 			tokens.targets.push(token.toUpperCase())
+		counts[token.length - 1]
 	}
 
-	select(tokens.piece)
-
-	const changes = {
-		remove: [],
-		move: {},
-		message: []
-	}
-
-	let target = tokens.targets.shift()
-	while (tokens.actions.length) {
-		const action = tokens.actions.shift()
-		const res = controller(tokens.piece, target, action)
-		if (!res.status) {
-			clear(res.message)
-			return;
-		}
-
-		if (res.remove)
-			changes.remove = changes.remove.concat(res.remove)
-		if (res.move)
-			changes.move = changes.move.concat(res.move)
-		changes.message.push(res.message)
-
-		if (res.piece)
-			tokens.piece = res.piece
-		if (tokens.targets.length)
-			target = tokens.targets.shift()
-	}
-
-	const fin_res = change_board(changes)
-	if (!fin_res.status) {
-		clear(fin_res.message)
+	if (counts[0] > counts[1]) {
+		clear(`Invalid maneuver: ${cmd}`)
 		return;
 	}
 
-	clear(changes.message.join('. '))
+	select(tokens.piece)
+	const res = controller(tokens)
+	change_board({ move, remove })
+	clear(res.message)
 })
